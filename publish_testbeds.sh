3769a2fcd03592b8dc7ac716e312baecb39dc64c
Sébastien Crozet <sebcrozet@dimforge.com>
2025-10-28 09:50:05 +0100

    fix testbed publish logic

---
+++ publish_testbeds.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+tmp=`mktemp -d`
+
+echo $tmp
+
+cp -r crates $tmp/.
+cp Cargo.toml $tmp/.
+
+cp -r src_testbed $tmp/crates/slosh_testbed2d/.
+cp -r LICENSE $tmp/crates/slosh_testbed2d/.
+cp -r README.md $tmp/crates/slosh_testbed2d/.
+cp -r shaders_testbed $tmp/crates/slosh_testbed2d/.
+
+cp -r src_testbed $tmp/crates/slosh_testbed3d/.
+cp -r LICENSE $tmp/crates/slosh_testbed3d/.
+cp -r README.md $tmp/crates/slosh_testbed3d/.
+cp -r shaders_testbed $tmp/crates/slosh_testbed3d/.
+
+# Publish slosh_testbed2d
+cd $tmp/crates/slosh_testbed2d
+ls
+sed 's#\.\./\.\./src_testbed#src_testbed#g' ./Cargo.toml > ./Cargo.toml.new
+mv Cargo.toml.new Cargo.toml
+sed 's#\.\./\.\./shaders_testbed#shaders_testbed#g' ./src_testbed/lib.rs > ./src_testbed/lib.rs.new
+mv src_testbed/lib.rs.new src_testbed/lib.rs
+cargo publish
+
+# Publish slosh_testbed3d
+cd ../slosh_testbed3d
+sed 's#\.\./\.\./src_testbed#src_testbed#g' ./Cargo.toml > ./Cargo.toml.new
+mv Cargo.toml.new Cargo.toml
+sed 's#\.\./\.\./shaders_testbed#shaders_testbed#g' ./src_testbed/lib.rs > ./src_testbed/lib.rs.new
+mv src_testbed/lib.rs.new src_testbed/lib.rs
+cargo publish
+
+# Cleanup
+rm -rf $tmp
+
